3823edcd639e972cd8e285cbf21c5085885b2e6b
max <2337833033@qq.com>
2025-07-21 00:28:16 +0800

    square方块类逻辑完善，clear后将原处渲染为棋盘颜色，避免直接擦除

--- src/core/square.ts
+++ src/core/square.ts
@@ -1,16 +1,20 @@
 import { ISquare } from './interfaces/ISquare';
 import { Point } from './types';
-import { COLORS } from '../config/shape.config';
-import { getRandomItem } from '../utils';
+import { getThemeColor } from '../utils';
+import { RenderUtils } from '../utils/renderUtils';
 import GAME_CONFIG from '../config/game.config';
 const { squareSize } = GAME_CONFIG;
 
 export class Square implements ISquare {
     private _ctx: CanvasRenderingContext2D | null = null;
-    private _point: Point | {} = {};
+    private _point: Point = { x: 0, y: 0 };
+    private _color: string = '';
+    private _renderUtils: RenderUtils | null = null;
 
-    constructor(ctx: CanvasRenderingContext2D) {
+    constructor(ctx: CanvasRenderingContext2D, color: string) {
         this._ctx = ctx;
+        this._color = color;
+        this._renderUtils = new RenderUtils(this._ctx);
     }
 
     setPoint(point: Point) {
@@ -22,21 +26,27 @@ export class Square implements ISquare {
     }
 
     remove(): void {
-        if (!this._ctx || Object.keys(this._point).length === 0) return;
-        const { x, y } = this._point as Point;
-        this._ctx.clearRect(x, y, squareSize, squareSize);
+        if (!this._ctx || (this._point.x === 0 && this._point.y === 0)) return;
+        this._ctx.clearRect(this._point.x, this._point.y, squareSize, squareSize);
+        this._renderUtils?.drawRect({
+            x: this._point.x,
+            y: this._point.y,
+            w: squareSize,
+            h: squareSize,
+            stokeColor: getThemeColor('drawLineColor'),
+            fillColor: getThemeColor('mainSectionBgc'),
+        });
     }
 
     private draw(): void {
         if (!this._ctx || Object.keys(this._point).length === 0) return;
-        const { x, y } = this._point as Point;
-        const bgColor = getRandomItem(COLORS);
-        this._ctx.save();
-        this._ctx?.beginPath();
-        this._ctx.moveTo(x, y);
-        this._ctx.rect(x, y, squareSize, squareSize);
-        this._ctx.fillStyle = bgColor;
-        this._ctx.fill();
-        this._ctx.restore();
+        this._renderUtils?.drawRect({
+            x: this._point.x,
+            y: this._point.y,
+            w: squareSize,
+            h: squareSize,
+            stokeColor: getThemeColor('drawLineColor'),
+            fillColor: this._color,
+        });
     }
 }
