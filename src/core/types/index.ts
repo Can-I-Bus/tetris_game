db4f4785f4e3d7771973244be423c34e19c269d7
max <2337833033@qq.com>
2025-07-28 13:04:28 +0800

    type分离

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -9,6 +9,54 @@ export type GameType = {
     onGamePause?: Function;
 };
 
+export type DrawLineType = {
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
+    color: string;
+    lineWidth: number;
+};
+
+export type DrawTextType = {
+    x: number;
+    y: number;
+    content: string;
+    color?: string;
+    fz: string;
+    textAlign?: CanvasTextAlign;
+    textBaseline?: CanvasTextBaseline;
+};
+
+export type DrawReduisRectType = {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    lineColor: string;
+    lineWidth: number;
+    radius: number;
+    fillColor?: string;
+};
+
+export type DrawRectType = {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    lineWidth?: number;
+    stokeColor?: string;
+    fillColor?: string;
+};
+
+export type DrawImgType = {
+    image: HTMLImageElement;
+    dx: number;
+    dy: number;
+    dWidth: number;
+    dHeight: number;
+};
+
 export type ActionButton = {
     name: string;
     imageSrc: string;
